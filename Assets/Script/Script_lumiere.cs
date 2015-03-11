80064d013c5972c13ab23330b02ed2a4bef7a850
Linheha <marjolaine.paz@hotmail.fr>
2015-03-11 19:40:34 +0100

    Ajout du dossier Unity

---
+++ Assets/Script/Script_lumiere.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Script_lumiere : MonoBehaviour
+{
+	public float allumage=0.01f;
+	public bool activage=false;
+
+	void Update ()
+		{
+		if (activage) {
+			light.intensity+=allumage;
+				}
+
+		}
+
+	void OnTriggerEnter(Collider other)
+	{
+		activage = true;
+	}
+	
+}
